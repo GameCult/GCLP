9101b7f6ae26cf91f9fba6471dc0a8d7ed262108
Maygician <maygicinc@gmail.com>
2020-10-03 13:32:06 +0700

    +statics

--- deploy/release.sh
+++ deploy/release.sh
@@ -2,7 +2,7 @@
 cd ../lp
 git pull origin pet
 mix deps.get --only prod
-# mix phx.digest # once you have something js related
+mix phx.digest # once you have something js related
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix release
 _build/prod/rel/lp/bin/lp start
