2d8a7c38e0d688eac5b1fd8e12d8f930e7515cdd
Maygician <maygicinc@gmail.com>
2020-10-05 22:20:08 +0700

    gh example implementation 0.0.1

--- lp/lib/lp_web/controllers/git_controller.ex
+++ lp/lib/lp_web/controllers/git_controller.ex
@@ -12,12 +12,40 @@ defmodule LpWeb.GitController do
     end
     def oauth(conn, %{"code" => code}) do
       #some persistance side effect
-      # persist(code)
-      render(conn, "oauth2.html", code: code)
+      # persist(code)        access_token = Github.Oauth2.Client.access_token!(
+          config: [client_id: @client_id, client_secret: @client_secret],
+          params: [code: code]
+        )
+
+        github_client = %Github.Client{access_token: access_token}
+
+        %{"login" => login} = github_client |> Github.Users.find!() |> Map.fetch!(:body)
+        [%{"email" => email} | _] = github_client |> Github.Users.Emails.list!() |> Map.fetch!(:body)
+      render(conn, "oauth2.html", [login: login, email: email])
       # redirect(conn, "git.html")
       # html(conn,"")
 
+
+      end
+
     end
+
+    # ___ example from github
+    # def github_callback(conn, %{"code" => code}) do
+    #   access_token = Github.Oauth2.Client.access_token!(
+    #     config: [client_id: @client_id, client_secret: @client_secret],
+    #     params: [code: code]
+    #   )
+
+    #   github_client = %Github.Client{access_token: access_token}
+
+    #   %{"login" => login} = github_client |> Github.Users.find!() |> Map.fetch!(:body)
+    #   [%{"email" => email} | _] = github_client |> Github.Users.Emails.list!() |> Map.fetch!(:body)
+    #   ...
+    # end
+
+    # ___
+
     def github(conn, _params) do
       authorize_url = Github.Oauth2.Client.authorize_url!(
         # config: [client_id: Application.fetch_env!(:lp, :client_id), client_secret: Application.fetch_env!(:lp, :client_secret)],
