efada54b3e7721805afe8a5d33cce5683a3ee6fa
Maygician <maygicinc@gmail.com>
2020-10-05 18:10:19 +0700

    sh path fixed

--- deploy/rel.sh
+++ deploy/rel.sh
@@ -12,8 +12,8 @@ echo 'export PORT=443' >> ~/.bashrc
 fi
 # and cert paths
 if [ -z "$GCLP_SSL_KEY_PATH"]; then 
-echo 'export GCLP_SSL_KEY_PATH=GCLP/lp/priv/cert/selfsigned_key.pem' >> ~/.bashrc
+echo 'export GCLP_SSL_KEY_PATH=/GCLP/lp/priv/cert/selfsigned_key.pem' >> ~/.bashrc
 fi
 if [ -z "$GCLP_APP_SSL_CERT_PATH"]; then 
-echo 'export GCLP_APP_SSL_CERT_PATH=GCLP/lp/priv/cert/selfsigned.pem' >> ~/.bashrc
+echo 'export GCLP_APP_SSL_CERT_PATH=/GCLP/lp/priv/cert/selfsigned.pem' >> ~/.bashrc
 fi
--- deploy/release.sh
+++ deploy/release.sh
@@ -7,5 +7,5 @@ npm install --prefix ./assets
 npm run deploy --prefix ./assets
 mix phx.digest # once you have something js related
 MIX_ENV=prod mix release
-_build/prod/rel/lp/bin/lp start
+sudo _build/prod/rel/lp/bin/lp start
 # note, that it is running in terminal 
